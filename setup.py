b7cf55469f740c2c63653564d78b75b0474fde31
unknown <kubotahockey@outlook.com>
2024-08-26 16:43:55 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 26 16:34:51 2024
+
+@author: brennan
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+      name = "KubotaWeb",
+      version = '0.1',
+      packages = find_packages(),
+      install_requires=[
+          'Flask>=1.1.2',
+          'pandas',
+          'numpy',
+          'random',
+          'stripe',
+          'io',
+          'csv',
+          ],
+      author = 'Brennan',
+      author_email= 'kubotahockey@outlook.com',
+      description= 'A web application for Kubota Website',
+      url= 'https://github.com/kubotahockey/kubota-hockey',
+      classifiers=[
+          'Programming Language:: Python :: 3',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          ],
+      python_requires = '>=3.6'
+      )
